de8818c00e53f405078f9b2212f8215e81c91c2c
Sam Wayne <samwayne11@gmail.com>
2018-10-31 16:48:30 -0600

    Photo requesting access

--- Post-Lit/ViewControllers/CameraViewController.swift
+++ Post-Lit/ViewControllers/CameraViewController.swift
@@ -9,23 +9,51 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    let captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        requestAuth()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func requestAuth() {
+        switch AVCaptureDevice.authorizationStatus(for: .audio) {
+        case .authorized: // The user has previously granted access to the camera.
+            print("Authorized")
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .audio) { granted in
+                if granted {
+                    print("Authorized")
+                }
+            }
+            
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+        }
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: // The user has previously granted access to the camera.
+            print("Authorized")
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    print("Authorized")
+                }
+            }
+            
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+        }
     }
-    */
-
+    func doStuff() {
+        
+    }
+    
+    
 }
